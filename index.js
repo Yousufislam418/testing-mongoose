5e3e8f71a9c727f1ad3f784318122f432e8ce51f
Yousufislam418 <yousufislamaps@gmail.com>
2025-06-17 00:43:06 +0300

    url edit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const port = process.env.PORT || 3000;
-require('dotenv').config();
 const User = require('./models/User');
 const app = express();
 
